c6f80ef7ef54d5ca3608b3f9cf791104619ada91
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-24 22:56:25 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.dispatch({
+    this.props.store.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
